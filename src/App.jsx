71b526747002d9c586f893bd0953d1682742ffd9
yahyakassab <ya7yakassab@gmail.com>
2023-01-29 14:35:10 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,12 @@ function App() {
       </div>
       <h1>Vite + gbas nesnas</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p className=" text-red-500">
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   )
 }
